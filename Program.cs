708433bf39308eff2de3e7f712411dc50dac5a53
cpbenipal <cpbenipal@gmail.com>
2023-09-24 09:45:58 +0530

    Devexprees setup

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Blazored.LocalStorage;
 using Blazored.Toast;
 using Cizaro_Blazor_Server.Data;
 using Cizaro_Blazor_Server.Services;
+using DevExpress.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddBlazoredLocalStorage();
@@ -17,7 +18,11 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddSingleton<AppState>();
 var configVal = builder.Configuration.GetValue<string>("AppSettings:CizaroApiBaseUrl");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(configVal) });
+builder.WebHost.UseWebRoot("wwwroot");
+builder.WebHost.UseStaticWebAssets();
+/* ... */
 
+builder.Services.AddDevExpressBlazor(configure => configure.BootstrapVersion = BootstrapVersion.v5);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
